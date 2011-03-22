16e514a468ef6bddc76508236e28e33fd9b4c27f
Charles Shen <shenzhaoyan@gmail.com>
2011-03-22 16:25:34 +0800

    complete translating english into chinese

--- lib/youdao_translate/command.rb
+++ lib/youdao_translate/command.rb
@@ -1,3 +1,4 @@
+# encoding: UTF-8
 require "optparse"
 require "ostruct"
 
@@ -11,33 +12,73 @@ module YoudaoTranslate
         opts.banner = "Usage: "
 
         opts.on_tail("-h", "--help", "Show this message") do
-          puts opts
+          STDOUT.puts(opts)
           exit
         end
 
-        options.basic = false
-        opts.on("-b", "--basic", "Get basic") do
-          options.basic = true
+        options.basic = true
+        opts.on("-b", "--[no-]basic", "基本释义") do
+          options.basic = false
+        end
+
+        options.phrase = false
+        opts.on("-r", "--phrase", "词组短语") do
+          options.phrase = true
+        end
+
+        options.synonym = false
+        opts.on("-n", "--synonym", "近义词") do
+          options.synonym = true
         end
 
         options.language = "eng"
-        opts.on("-l", "--language OPT", ["eng", "fr", "chn"], "Set language, default english") do |language|
+        opts.on("-l", "--language OPT", ["eng", "fr", "chn"], "设置语言，默认翻译为英语") do |language|
           options.language = language
         end
 
+        options.web = false
+        opts.on("-w", "--web", "网络释义") do
+          options.web = true
+        end
+
+        options.powertrans = false
+        opts.on("-p", "--powertrans", "21世纪大英汉词典") do
+          options.powertrans = true
+        end
+
+        options.eetrans = false
+        opts.on("-e", "--eetrans", "英英释义") do
+          options.eetrans = true
+        end
+
+        options.bilingual = false
+        opts.on("-g", "--bilingual", "双语例句") do
+          options.bilingual = true
+        end
+
         options.original = false
-        opts.on("-o", "--original", " ") do
+        opts.on("-o", "--original", "原声例句") do
           options.original = true
         end
 
+        options.authority = false
+        opts.on("-a", "--authority", "权威例句") do
+          options.authority = true
+        end
+
         options.baike = false
-        opts.on("-k", "--baike", " ") do
+        opts.on("-k", "--baike", "百科") do
           options.baike = true
         end
 
         options.sentence = false
         opts.on("-s", "--sentence", " ") do
           options.sentence = true
+        end
+
+        opts.on("-v", "--version", "版本号") do
+          STDOUT.puts(VERSION)
+          exit
         end
 
       end
--- lib/youdao_translate/youdao_word.rb
+++ lib/youdao_translate/youdao_word.rb
@@ -1,58 +1,118 @@
+# encoding: UTF-8
 module YoudaoTranslate
   class YoudaoWord
     include Command
+    ATTRIBUTES = { "Standard" => 0, "Bold" => 1, "Reverse" => 2 }
+    COLORS = { "Black" => 30, "Red" => 31, "Green" => 32, "Yellow" => 33, "Blue" => 34, "Magenta" => 35, "Cyan" => 36,  "White" => 37 }
 
-    def self.search(q, basic = false, web = false, power = false, bilingual = false, original = false, baike = false, language = "eng")
-      path = "http://dict.youdao.com/search?le=#{language}&q=#{CGI.escape(q)}&ue=utf8"
+    def self.search(q, options)
+      path = "http://dict.youdao.com/search?le=#{options[:language]}&q=#{CGI.escape(q)}&ue=utf8"
       page = Nokogiri::HTML(open(path))
 
-      if basic
-        basic_area = page.at("#results #eTransform ul li")
-        basic_area && basic = basic_area.text.gsub(/\n\s+/, " ")
-        p basic
+      page.css("p.via", "p.example-via").each{ |element| element.remove }
+
+      if options[:basic]
+        announce_area = page.at("#results .trans-wrapper h2")
+        announce_area && announce = announce_area.text
+        STDOUT.puts set_color("", announce, "Red") if announce
+
+        basic_text = []
+        basic_area = page.css("#results #eTransform #etcTrans ul li")
+        basic_area && basic_area.each do |ba|
+          basic_text << ba.text
+        end
+        STDOUT.puts set_color("基本释义:\n", basic_text, "Blue")
       end
 
-      if web
+      if options[:phrase]
+        phrases = []
+        phrase_area = page.css("#results #eTransform #wordGroup .wordGroup")
+        phrase_area && phrase_area.each do |pa|
+          phrases << pa.text
+        end
+        STDOUT.puts set_color("词组短语:\n", phrases, "Yellow")
+      end
+
+      if options[:synonym]
+        synonyms = []
+        synonyms_area = page.css("#results #eTransform #synonyms ul li")
+        synonyms_area && synonyms_area.each do |sa|
+          synonyms << sa.text
+        end
+        STDOUT.puts set_color("近义词:\n", synonyms, "Yellow")
+      end
+
+      if options[:web]
         web_trans = []
         web_trans_area = page.css("#results #tWebTrans #webPhrase .wordGroup")
         web_trans_area && web_trans_area.each do |wt|
-          web_trans << wt.text.gsub(/\n\s+/, " ")
+          web_trans << wt.text
         end
-        p web_trans
+        STDOUT.puts set_color("网络释义:\n", web_trans, "Yellow")
       end
 
-      if power
+      if options[:powertrans]
         power_trans = []
-        power_trans_area = page.css("#results #tPowerTrans ul.ol li")
+        power_trans_area = page.css("#results #tPowerTrans ul li span.def")
         power_trans_area && power_trans_area.each do |pt|
-          power_trans << pt.text.gsub(/\n\s+/, " ")
+          power_trans << pt.text
         end
-        p power_trans
+        STDOUT.puts set_color("21世纪大英汉词典:\n", power_trans, "Blue")
       end
 
-      if bilingual
+      if options[:eetrans]
+        eetrans_trans = []
+        eetrans_trans_area = page.css("#results #tEETrans ul li span.def")
+        eetrans_trans_area && eetrans_trans_area.each do |et|
+          eetrans_trans << et.text
+        end
+        STDOUT.puts set_color("英英释义:\n", eetrans_trans, "Yellow")
+      end
+
+      if options[:bilingual]
         bilinguals = []
-        bilinguals_area = page.css("#results #examples #bilingual ul.ol li p")
+        bilinguals_area = page.css("#results #examples #bilingual ul li p")
         bilinguals_area && bilinguals_area.each do |b|
-          bilinguals << b.text.gsub(/\n\s+/, " ")
+          bilinguals << b.text
         end
-        p bilinguals
+        STDOUT.puts set_color("双语例句:\n", bilinguals, "Blue")
       end
 
-      if original
+      if options[:original]
         original_sounds = []
-        original_sounds_area = page.css("#results #examples #originalSound ul.ol li p")
+        original_sounds_area = page.css("#results #examples #originalSound ul li p")
         original_sounds_area && original_sounds_area.each do |os|
-          original_sounds << os.text.gsub(/\n\s+/, " ")
+          original_sounds << os.text
         end
-        p original_sounds
+        STDOUT.puts set_color("原声例句:\n", original_sounds, "Yellow")
       end
 
-      if baike
-        baike_area = page.at_css("#results #eBaike .content")
-        baike_area && (baike = baike_area.text.gsub(/\n\s+/, " "))
-        p baike_area
+      if options[:authority]
+        authority_trans = []
+        authority_trans_area = page.css("#results #examples #authority ul li p:nth-child(odd)")
+        authority_trans_area && authority_trans_area.each do |at|
+          authority_trans << at.text
+        end
+        STDOUT.puts set_color("权威例句:\n", authority_trans, "Blue")
+      end
+
+      if options[:baike]
+        baike_text = []
+        baike_area = page.css("#results #eBaike .content p")
+        baike_area && baike_area.each do |ba|
+          baike_text << ba.text
+        end
+        STDOUT.puts set_color("百科:\n", baike_text, "Green")
       end
     end
+
+    def self.set_color(header, text, color, bold = true)
+      text = text.is_a?(Array) ? text.each{|t| t.gsub!(/\n\s+/, " ")}.join("\n") : text.gsub!(/\n\s+/, " ")
+
+      bold = bold ? ATTRIBUTES["Bold"] : ATTRIBUTES["Standard"]
+      color = COLORS[color.to_s.capitalize] || COLORS["Green"]
+      "\033[#{bold};#{color}m#{header}#{text}\033[0m"
+    end
+
   end
 end
